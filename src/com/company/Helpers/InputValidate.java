ff5694e52405d3399afa5e4a2beb0126b6cea3d3
artowir97 <artowir9@wp.pl>
2022-01-04 15:31:39 +0100

    HomeWorks v2.1.2 Fix: InputValidate - rename variables to loverCase and fix typo in them.

--- src/com/company/Helpers/InputValidate.java
+++ src/com/company/Helpers/InputValidate.java
@@ -2,7 +2,6 @@ package com.company.Helpers;
 //Scanner
 import java.util.Scanner;
 //TO-DO
-//TODO: change varibles name to cammelcase
 //TODO: Add isChar Validation
 //TODO: Add extended Patterns to isString func and refactor this.
 public class InputValidate {
@@ -37,7 +36,7 @@ public class InputValidate {
         return input;
     }
 
-    public int isInt (String message, String error_message, boolean Positive, boolean Limit, int[] Between, boolean Unical, int[] unicalList) { //, boolean isPositive
+    public int isInt (String message, String error_message, boolean positive, boolean limit, int[] between, boolean unique, int[] uniqueList) { //, boolean isPositive
         boolean isValid = false;
         if (error_message == null) error_message = "Invalid input, number expected";
         //find between java function
@@ -47,39 +46,39 @@ public class InputValidate {
         //big validate
         do {
             System.out.print(message);
-            Scanner sc = new Scanner(System.in); //imput value and start validate
-            if (sc.hasNextInt() && Positive && Limit && Unical) {
+            Scanner sc = new Scanner(System.in); //input value and start validate
+            if (sc.hasNextInt() && positive && limit && unique) {
                 input = sc.nextInt();
                 if (input > 0) {
-                    if (input >= Between[0] && input <= Between[1]) {
-                        if (isUnical(input, unicalList)){
+                    if (input >= between[0] && input <= between[1]) {
+                        if (isUnique(input, uniqueList)){
                             isValid = true;
                         }else {
-                            System.out.print("Invalid input, unical value different them (");
-                            for(int listItem : unicalList ) {System.out.print(listItem+" ");}
+                            System.out.print("Invalid input, unique value different them (");
+                            for(int listItem : uniqueList ) {System.out.print(listItem+" ");}
                             System.out.println(") expected.");
                         }
-                    }else System.out.println("Invalid input, positive number between "+Between[0]+" and "+Between[1]+" expected");
+                    }else System.out.println("Invalid input, positive number between "+between[0]+" and "+between[1]+" expected");
                 }else {
                     System.out.println("Invalid input, positive number expected");
                 }
-            }else if (sc.hasNextInt() && Positive && Limit) {
+            }else if (sc.hasNextInt() && positive && limit) {
                 input = sc.nextInt();
                 if (input > 0) {
-                    if (input >= Between[0] && input <= Between[1]) {
+                    if (input >= between[0] && input <= between[1]) {
                         isValid = true;
-                    }else System.out.println("Invalid input, positive number between "+Between[0]+" and "+Between[1]+" expected");
+                    }else System.out.println("Invalid input, positive number between "+between[0]+" and "+between[1]+" expected");
                 }else {
                     System.out.println("Invalid input, positive number expected");
                 }
-            }else if (sc.hasNextInt() && Limit)  {
+            }else if (sc.hasNextInt() && limit)  {
                 input = sc.nextInt();
-                if (input >= Between[0] && input <= Between[1]) {
+                if (input >= between[0] && input <= between[1]) {
                     isValid = true;
                 }else {
-                    System.out.println("Invalid input, number between "+Between[0]+" and "+Between[1]+" expected");
+                    System.out.println("Invalid input, number between "+between[0]+" and "+between[1]+" expected");
                 }
-            }else if (sc.hasNextInt() && Positive) {
+            }else if (sc.hasNextInt() && positive) {
                 input = sc.nextInt();
                 if (input > 0) {
                     isValid = true;
@@ -96,19 +95,16 @@ public class InputValidate {
         return input;
     }
 
-    public boolean isUnical (int currentValue,int[] valueList ) {
-        int x;
-        boolean isUnical = false;
+    public boolean isUnique (int currentValue,int[] valueList ) {
+        boolean isUnique = false;
 
         for (int item :  valueList) {
             if (currentValue == item) {
-                isUnical = false;
-                return  isUnical;
+                return  false;
             }else {
-                isUnical = true;
+                isUnique = true;
             }
         }
-
-        return isUnical;
+        return isUnique;
     }
 }
