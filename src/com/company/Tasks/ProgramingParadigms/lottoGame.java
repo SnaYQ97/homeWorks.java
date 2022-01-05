172c5d0f5f5e11f1a76c6a1c17002739d70e0514
artowir97 <artowir9@wp.pl>
2022-01-05 22:27:33 +0100

    HomeWorks v2.4.6 Upgrade: rename ArrayHelper to IntArrayHelper

--- src/com/company/Tasks/ProgramingParadigms/lottoGame.java
+++ src/com/company/Tasks/ProgramingParadigms/lottoGame.java
@@ -3,7 +3,7 @@ package com.company.Tasks.ProgramingParadigms;
 import java.util.Arrays;
 
 import com.company.Helpers.InputValidate;
-import com.company.Helpers.ArrayHelper;
+import com.company.Helpers.IntArrayHelper;
 
 public class lottoGame {
     public void task() {
@@ -12,11 +12,11 @@ public class lottoGame {
         int dayCounter = 0;
 
         firstNumber = new InputValidate().isInt("Input first number (0:49): ", null, true, true, Limit, false, null);
-        lottoTicket = new ArrayHelper().addElement(firstNumber, lottoTicket);
+        lottoTicket = new IntArrayHelper().addElement(firstNumber, lottoTicket);
 
         while (lottoTicket.length <= 5) {//take 6 unique numbers from user
             nextNumber = new InputValidate().isInt("Input unique number (0:49): ", null, true, true, Limit, true, lottoTicket);
-            lottoTicket = new ArrayHelper().addElement(nextNumber, lottoTicket);
+            lottoTicket = new IntArrayHelper().addElement(nextNumber, lottoTicket);
         }
         Arrays.sort(lottoTicket);
 
@@ -42,10 +42,10 @@ public class lottoGame {
         while ((lotto.length <= 5)) {
             lottoNumber = getRandomIntBetween(1, 50);
             if (lotto.length == 0) {
-                lotto = new ArrayHelper().addElement(lottoNumber, lotto);
+                lotto = new IntArrayHelper().addElement(lottoNumber, lotto);
             } else {
                 if (new InputValidate().isUnique(lottoNumber, lotto)) {
-                    lotto = new ArrayHelper().addElement(lottoNumber, lotto);
+                    lotto = new IntArrayHelper().addElement(lottoNumber, lotto);
                 }
             }
         }
