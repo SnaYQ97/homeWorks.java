46e62d2813c75dd7c5fef2707a346e5c70241962
artowir97 <artowir9@wp.pl>
2022-01-05 20:19:58 +0100

    HomeWorks v2.3.1 Fix substringCounter

--- src/com/company/Tasks/BasicOfPrograming/substringCounter.java
+++ src/com/company/Tasks/BasicOfPrograming/substringCounter.java
@@ -1,6 +1,7 @@
 package com.company.Tasks.BasicOfPrograming;
 
 import java.util.Scanner;
+import com.company.Helpers.InputValidate;
 
 public class substringCounter {
     public void task() {
@@ -14,38 +15,36 @@ public class substringCounter {
     public static void wordsFromSentence(Scanner sentence) {
         int words = 0, letters = 0, lines = 0, wordRepeating = 0;
         String word;
-        String sentenceLine, lookingWord="";
+        String sentenceLine, lookingWord;
 
         if (sentence.hasNextLine()) {
-            System.out.println("Input word for looking in sentence.");
-            Scanner userWordInput = new Scanner(System.in);
             System.out.println("For enter press Ctrl+D or Ctrl+C.");
-            lookingWord = userWordInput.nextLine();
-        }
-
-
-        while (sentence.hasNextLine()) {
-            sentenceLine = sentence.nextLine();
-            //System.out.println(sentenceLine);
-            Scanner line = new Scanner(sentenceLine);
-            letters += countLength(sentenceLine);
-            lines++;
-            while (line.hasNext()) {
-                word = line.next();
-                words++;
-                if (word.contains(lookingWord)) {
-                    wordRepeating++;
+            lookingWord = new InputValidate().isString("Input word for looking in sentence.", null, false, null);
+            if (lookingWord.length() > 0) {
+                while (sentence.hasNextLine()) {
+                    sentenceLine = sentence.nextLine();
+                    //System.out.println(sentenceLine);
+                    Scanner line = new Scanner(sentenceLine);
+                    letters += countLength(sentenceLine);
+                    lines++;
+                    while (line.hasNext()) {
+                        word = line.next();
+                        words++;
+                        if (word.contains(lookingWord)) {
+                            wordRepeating++;
+                        }
+                    }
+                    if (!sentence.hasNextLine()) {
+                        System.out.println();
+                        System.out.println("--------STATISTICS---------");
+                        System.out.println("Words: "+words);
+                        System.out.println("Letters: " + letters);
+                        System.out.println("Lines: " + lines);
+                        System.out.println(lookingWord + " repeating: " + wordRepeating);
+                    }
                 }
-            }
-            if (!sentence.hasNextLine()) {
-                System.out.println();
-                System.out.println("--------STATISTICS---------");
-                System.out.println("Words: "+words);
-                System.out.println("Letters: " + letters);
-                System.out.println("Lines: " + lines);
-                System.out.println(lookingWord + " repeating: " + wordRepeating);
-            }
-        }
+            }else return;
+        }else return;
     }
 
     public static int countLength(String input) {
