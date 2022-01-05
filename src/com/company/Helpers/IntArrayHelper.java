ac663a9d6b5b259d4b9a62878899bb788063463c
artowir97 <artowir9@wp.pl>
2022-01-05 22:26:57 +0100

    HomeWorks v2.4.3 Upgrade: rename ArrayHelper to IntArrayHelper and use System.arraycopy

--- src/com/company/Helpers/IntArrayHelper.java
+++ src/com/company/Helpers/IntArrayHelper.java
@@ -1,15 +1,13 @@
 package com.company.Helpers;
 
-public class ArrayHelper {
+public class IntArrayHelper {
     public int[] addElement(int newValue, int[] oldArray) {
         int[] newArray = new int[oldArray.length+1]; //create new arr with one more cell them old one
 
-        for(int x = 0; x < oldArray.length; x++) {
-            newArray[x] = oldArray[x]; //copy old arr to new
-        }
+        //copy old arr to new
+        System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
         newArray[oldArray.length] = newValue; //put new value to last cell in new array
 
         return newArray;
     }
-
 }
